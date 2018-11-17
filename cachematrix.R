bf6ec2344f428b66dbbe3edbebc7d28c4ee0db53
passw0rt <44312274+passw0rt@users.noreply.github.com>
2018-11-17 12:31:15 -0800

    Submission of Programming Assignment 2 on Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## The following pair of functions, makeCacheMatrix and cacheSolve, cache the inverse of a given invertible square matrix.
 
-## The makeCacheMatrix function maps each invertible square matrix to a list of four functions, set, get, setInv, and getInv, for getting and setting the invertible square matrix stored within the parent environment of the functions as well as getting and setting the matrix identified as its inverse.  The default argument of makeCacheMatrix is presumed to be the 1x1 unit matrix when no invertible square matrix is specified. 
+## The makeCacheMatrix function maps each invertible square matrix to a list of four functions, set, get, setInv, and getInv, 
+## for getting and setting the invertible square matrix stored within the parent environment of the functions as well as 
+## getting and setting the matrix identified as its inverse.  
+## The default argument of makeCacheMatrix is presumed to be the 1x1 unit matrix when no invertible square matrix is specified. 
 
 makeCacheMatrix <- function(thisMatrix = matrix(data=1,nrow=1,ncol=1)) 
 {
@@ -35,7 +38,9 @@ makeCacheMatrix <- function(thisMatrix = matrix(data=1,nrow=1,ncol=1))
 }
 
 
-## The cacheSolve function returns the inverse matrix of the invertible square matrix stored in the parent environment of the list of functions returned by calling makeCacheMatrix. The cacheSolve function calculates the inverse matrix of the invertible square matrix and stores it in the parent environment of the functions only if no inverse matrix has been stored in the parent environment.
+## The cacheSolve function returns the inverse matrix of the invertible square matrix stored in the parent environment of the list
+## of functions returned by calling makeCacheMatrix. The cacheSolve function calculates the inverse matrix of the invertible square
+## matrix and stores it in the parent environment of the functions only if no inverse matrix has been stored in the parent environment.
 
 cacheSolve <- function(cacheMatrix, ...) 
 {
